c6723ad2a81a310ef231669191bec7f22f7870ea
Pritam Das <dpritam28577@gmail.com>
2023-08-04 11:05:56 +0530

    Schema has been created

--- model/url.js
+++ model/url.js
@@ -1,1 +1,25 @@
-const mongoose = require("mon")
+const mongoose = require("mongoose");
+
+
+const urlSchema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    redirectUrl: {
+        type: String,
+        required: true,
+    },
+    visiteHistory: [{
+        timestap: {
+            type: Number
+        }
+    }]
+
+
+}, { timestamps: true });
+
+const URL = mongoose.model("url",urlSchema);
+
+module.exports = URL;
