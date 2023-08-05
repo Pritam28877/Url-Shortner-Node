74f8cda754bf8494ff2128d06a4ef6e96ee4831e
Pritam Das <dpritam28577@gmail.com>
2023-08-05 13:03:08 +0530

    add the redirectUrl logic and add the debugger

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const urlRoute = require("./router/urlRoutes");
+const URL = require("./model/url")
 const db = require ('./config/mongodb')
 const app = express();
 const port = 5000 ;
+
+
+app.use(express.json());
 
 app.get('/',(req , res)=>{
     res.send("hello");
 })
 app.use("/url", urlRoute)
+
+app.get('/:shortId',async(req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {shortId} , {
+            $push :{
+                visiteHistory :{
+                    timestap : Date.now(),
+                }
+            }
+        });
+        res.redirect(entry.redirectUrl);
+})
 app.listen(port , (e)=>{
     if(e){
         return console.log("the error :" + e);
